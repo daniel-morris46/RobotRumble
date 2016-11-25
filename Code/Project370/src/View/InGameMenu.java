e7d11c312dd70d00cd434d1dfdd35fba9ebed4b3
bmj920 <bmj920@usask.ca>
2016-11-25 00:34:23 -0600

    Initialized JButtons in constructor
    Initialized all JButtons in the constructor, attatching their action listeners and setting them as visible.

--- Code/Project370/src/View/InGameMenu.java
+++ Code/Project370/src/View/InGameMenu.java
@@ -1,5 +1,8 @@
 package View;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class InGameMenu extends JFrame{
@@ -20,10 +23,93 @@ public class InGameMenu extends JFrame{
         
         setSize(1000, 1000);
         setVisible(true);
+        
+        JPanel gamePanel = new JPanel();
+        gamePanel.setSize(getWidth(), getHeight());
+        
+        actionToggleButton = new JButton("*");	//Creating action toggle button and adding it to panel
+        actionToggleButton.addActionListener(new ActionToggleButtonListener());
+        gamePanel.add(actionToggleButton);
+        actionToggleButton.setVisible(true);
+        
+        leftButton = new JButton("<");			//Creating left button and adding it to panel
+        leftButton.addActionListener(new LeftButtonListener());
+        gamePanel.add(leftButton);
+        leftButton.setVisible(true);
+        
+        rightButton = new JButton(">");			//Creating right button and adding it to panel
+        rightButton.addActionListener(new RightButtonListener());
+        gamePanel.add(rightButton);
+        rightButton.setVisible(true);
+
+        actionButton = new JButton("Move");		//Creating action button and adding it to panel
+        actionButton.addActionListener(new ActionButtonListener());
+        gamePanel.add(actionButton);
+        actionButton.setVisible(true);
+        
+        endPlayButton = new JButton("End Play");//Creating end play button and adding it to panel
+        endPlayButton.addActionListener(new EndPlayButtonListener());
+        gamePanel.add(endPlayButton);
+        endPlayButton.setVisible(true);
+        
+        forfeitButton = new JButton("Forfeit");	//Creating forfeit button and adding it to panel
+        forfeitButton.addActionListener(new ForfeitButtonListener());
+        gamePanel.add(forfeitButton);
+        forfeitButton.setVisible(true);
+        
+        exitButton = new JButton("Exit");		//Creating exit button and adding it to panel
+        exitButton.addActionListener(new ExitButtonListener());
+        exitButton.setVisible(true);
+        gamePanel.add(exitButton);
+        
+        this.add(gamePanel);					//Add the game panel to the JFrame
+        gamePanel.setVisible(true);				//Set the game panel as visible
     }
     
     public static void main(String[] args){
     	InGameMenu menu = new InGameMenu(5);
     }
 
+
+	private class ActionToggleButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+
+    	}
+    }
+	
+	private class LeftButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
+	
+	private class RightButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
+	
+	private class ForfeitButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+
+    	}
+    }
+	
+	private class ExitButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
+	
+	private class ActionButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
+	
+	private class EndPlayButtonListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
 }
