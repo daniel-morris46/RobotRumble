d8a9cd8431f4acdc43384dc836824ddefdb03282
bmj920 <bmj920@usask.ca>
2016-11-25 00:55:52 -0600

    Created hexagon grid
    Created hexagonal grid that is generated based on side size.

--- Code/Project370/src/View/InGameMenu.java
+++ Code/Project370/src/View/InGameMenu.java
@@ -1,5 +1,9 @@
 package View;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -9,7 +13,6 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import View.InGameMenu.Robot;
 
 public class InGameMenu extends JFrame{
 
@@ -37,7 +40,7 @@ public class InGameMenu extends JFrame{
     }
     
     public static void main(String[] args){
-    	InGameMenu menu = new InGameMenu(5);
+    	InGameMenu menu = new InGameMenu(7);
     }
     
     public void remakeBoard(int size){
@@ -142,10 +145,28 @@ public class InGameMenu extends JFrame{
 		
 		/** @public Integer reference to the current robot to be moved */
 		public int currentRobotIndex;
+		
+		/** @category Hex Variables */
+		
+		/** @private the side length of each hexagon in pixels */ 
+		private int s = 0;
+		
+		/** @private t = The vertical gap between corner points */
+		private int t = 0;
+		
+		/** @private r = The horizontal gap between corner points */
+		private int r = 0;
+		
+		/** @private h = Two times the vertical gap */
+		private int h = 0;
+		
+		/**@private Gap between board and top of frame in pixels*/
+		private int PIXELBORDER = 100;
 			
 		public DrawingPanel(int size){
 			boardSize = size;
 			walkable = new Boolean[size * 2][size * 2];
+			setBySide(30);
 			
 			for(int i = 0; i < size; i++)
 				for(int j = 0; j < size; j++)
@@ -156,7 +177,128 @@ public class InGameMenu extends JFrame{
 			
 			for(int i = 0; i < 18; i++){	//Initialize each robot in the array
 				robot[i] = new Robot(boardSize - 1, boardSize - 1);
+			}
 		}
+			
+
+			@Override
+	    	public void paintComponent(Graphics g){
+	    		Graphics2D g2 = (Graphics2D) g;
+	    		
+	    		super.paintComponent(g2);
+	    		
+	    		
+	    		for(int i = 0; i < boardSize * 2 - 1; i++){		// i = 0, 1, ... boardSize * 2 - 1    			
+	    														//Hexagons to initialize will depend on which sections are being made
+	    			if(i < boardSize){							//If we are before the halfway mark
+	    														//j = boardSize - 1 ... boardSize * 2
+	    				for(int j = boardSize - (i + 1); j < (boardSize * 2) - 1 ; j++){
+		    				walkable[i][j] = true;
+		        			drawHex(i,j,g2);
+		        			//System.out.println("I: " + i + "  J: " + j);
+		    			}
+	    			} else {									//Otherwise, we are halfway or past
+	    														//j = i / boardSize ... (boardSize * 2) - (i % boardSize - 1)
+	    				for(int j = (i / boardSize) - 1; j < (boardSize * 2) - (i % boardSize) - 2; j++){
+	    					walkable[i][j] = true;
+		        			drawHex(i,j,g2);
+		    			}
+	    			}
+	    		}
+	    		
+	    		for(int i = 0; i < 18; i++){		//Draw each robot on the booard
+	    			drawRobot(robot[i], g2);
+	    			
+	    		}
+	    	}
+			
+			/**
+		     * BOARD CONSTRUCTION FUNCTIONS
+		     */
+			
+			
+			/**
+			 * Sets the hexagon variables by 
+			 * @param sideLength
+			 */
+		    private void setBySide(int sideLength){
+		    	s = sideLength;							//s = the side length of each hex in pixels
+		    	t = (int) (s / 2);						//t = s * sin(30)
+		    	r = (int) (s * Math.cos(Math.PI / 6));	//r = s * cos(30)
+		    	h = 2 * r;								//
+		    }
+		    
+		    private Polygon createHex(int xPos, int yPos){
+
+		    	int x = xPos + PIXELBORDER;		//Current x equals the xPos + the border size
+		    	int y = yPos + PIXELBORDER;		//Current y equals the yPos + the border size
+		    	
+		    	int[] cx, cy;					//Arrays for x and y point coordinates
+		    	
+		    	cx = new int[] {x, x, x+r, x+r+r, x+r+r, x+r};			//Begins with the bottom left point and generates clockwise
+		    	cy = new int[] {y+t, y+s+t, y+s+t+t, y+s+t, y+t, y};	//Begins with the bottom left point and generates clockwise
+		    	
+		    	return new Polygon(cx, cy, 6);	//Returns the created hexagon
+		    }
+		    
+			/**
+	    	 * Private function for drawing a hexagon on a Graphics2D object
+	    	 * 
+	    	 * @param i The array x-position of the hexagon
+	    	 * @param j The array y-position of the hexagon
+	    	 * @param g The Graphics2D object to draw the hex on 
+	    	 */
+	    	private void drawHex(int i, int j, Graphics2D g){
+	    	    	int x = i * (s + t);
+	    	    	int y = j * h + i * (h / 2);
+	    	    	Polygon poly = createHex(y, x);
+	    	    	g.setColor(hexColor(i, j));
+	    	    	g.fillPolygon(poly);
+	    	    	g.setColor(Color.BLACK);
+	    	    	g.drawPolygon(poly);
+	    	}
+	    	
+	    	/**
+	    	 * Gets the current hex color to draw based on the board size
+	    	 * 
+	    	 * @param x The hex's x value
+	    	 * @param y The hex's y value
+	    	 * 
+	    	 * @return The color to paint the hex
+	    	 */
+	    	private Color hexColor(int x, int y){
+	    		Color color = Color.white;				//Default hex is white
+	    		
+	    		if(x == boardSize - 1 && y == 0)		//West side is red
+	    			color = Color.red;
+	    		else if(x == 0 && y == boardSize - 1)		//North-West side is orange
+	    			color = Color.orange;
+	    		else if (x == 0 && y == boardSize * 2 - 2)
+	    			color = Color.yellow;
+	    		else if (x == boardSize - 1 && y == boardSize * 2 - 2)
+	    			color = Color.green;
+	    		else if (x == boardSize * 2 - 2 && y == 0)
+	    			color = Color.pink;
+	    		else if (x == boardSize * 2 - 2 && y == boardSize - 1)
+	    			color = Color.blue;
+	    		
+	    		return color;
+	    	}
+	    	
+	    	/**
+	    	 * Private function for drawing a robot on the Graphics2D object
+	    	 * 
+	    	 * @param toDraw The robot to draw
+	    	 * @param g	The Graphics2D object to draw on
+	    	 */
+	    	private void drawRobot(Robot toDraw, Graphics2D g){
+		    	int y = toDraw.currentX * (s + t);
+		    	int x = toDraw.currentY * h + toDraw.currentX * (h / 2);
+		    	
+		    	x += PIXELBORDER;											//Current x equals the xPos + the border size
+		    	y += PIXELBORDER;											//Current y equals the yPos + the border size
+		    	g.drawImage(toDraw.image, x + r / 2, y + s / 2, gamePanel);
+	    	}
 	 }
 	 
 	 
@@ -180,7 +322,6 @@ public class InGameMenu extends JFrame{
 	    		currentX = xPos;
 	    		currentY = yPos;
 	    		
-	    		System.out.println("Current Robot Position: x = " + currentX + " y = " + currentY);
 	    	}
 	    	
 	    	/** Moves the robot one space forward based on the current direction */
