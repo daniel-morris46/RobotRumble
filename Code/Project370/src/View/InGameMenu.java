ec2a1fda42722f3b9e8d3f804ed022729d348b41
bmj920 <bmj920@usask.ca>
2016-11-25 00:26:00 -0600

    Added variables for JButtons
    Added variables for the JButtons the class will need.

--- Code/Project370/src/View/InGameMenu.java
+++ Code/Project370/src/View/InGameMenu.java
@@ -1,9 +1,29 @@
 package View;
 
-public class InGameMenu {
+import javax.swing.*;
 
-    public InGameMenu() {
-        // TODO Auto-generated constructor stub
+public class InGameMenu extends JFrame{
+
+	
+	public JButton leftButton;
+	public JButton rightButton;
+	public JButton forfeitButton;
+	public JButton exitButton;
+	public JButton actionToggleButton;
+	public JButton actionButton;
+	public JButton endPlayButton;
+	
+    public InGameMenu(int size) {
+        super("Super Robot Rumble 370");
+        
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+        setSize(1000, 1000);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+    	InGameMenu menu = new InGameMenu(5);
     }
 
 }
