b50b58b345ca4e0ca344a36cd009e8e4d24a79e4
Daniel Morris (ddm855) <ddm855@mail.usask.ca>
2016-11-24 18:38:39 -0600

    Adding spectator skeleton

---
+++ Code/Model/Spectator.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Spectator {
+
+    public Spectator() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
