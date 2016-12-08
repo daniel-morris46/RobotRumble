ec2c461f34b615603a792ff0f33965bb2bd74ae3
bmj920 <bmj920@usask.ca>
2016-12-07 18:46:07 -0600

    Updated OptionsMenu
    Updated the options menu to work better with the revised OptionsMenuPanel.

--- Code/Project370/src/View/OptionsMenu.java
+++ Code/Project370/src/View/OptionsMenu.java
@@ -1,13 +1,18 @@
 package View;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import Controller.Controller;
+
 public class OptionsMenu extends JFrame{
-	
-	
-	
+
 	/**@public The panel where all images will be drawn*/
-	public OptionsMenuPanel gamePanel;
+	public OptionsMenuPanel optionsPanel;
 	
 	private static final long serialVersionUID = 1L;
 
@@ -17,9 +22,32 @@ public class OptionsMenu extends JFrame{
 	 * Constructs the frame for the options menu.
 	 */
 	public OptionsMenu() {
-        super("370 ROBOT RUMBLE");
+        super("Import robots");
+        setLayout(new GridLayout(2,1));
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+       
+        setSize(1400, 1000);
+        optionsPanel = new OptionsMenuPanel();
+        add(optionsPanel);
+        optionsPanel.setVisible(true);
         setVisible(true);
-        setSize(1400,  1000);
+        
+        JButton backButton  = new JButton("Back");
+        backButton.addActionListener(new BackListener());
+        add(backButton);
+        revalidate();
     }
+	
+    /** 
+     * The action listener for the back button.
+     */
+    private class BackListener implements ActionListener{
+    	public void actionPerformed(ActionEvent e){
+    		
+    	}
+    }
+	
+	public static void main(String args[]){
+		new OptionsMenu();
+	}
 }
