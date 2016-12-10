9c50b29c9dba5f658381ed121ec48f7c1f7d94a9
Jack Huang (yuh992) <yuh992@mail.usask.ca>
2016-12-10 00:30:44 -0600

    Added Mail box in robot team

--- Code/Project370/src/Model/RobotTeam.java
+++ Code/Project370/src/Model/RobotTeam.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.awt.Color;
+import java.util.*;
 
 public class RobotTeam {
 
@@ -16,11 +17,17 @@ public class RobotTeam {
     /** The index number of the team. */
     int number;
     
+    LinkedList<Robot> mailBoxdata;
+    
+    LinkedList<Integer> turnNumber;
+    
     /** Constructs a robot team given a boolean for if the team is human, the colour, and the index number of the team. */
     public RobotTeam(boolean isHmn, Color teamColour, int teamNumber) {
         isHuman = isHmn;
         colour = teamColour;
         teamOfRobot = new Robot[3];
+        mailBoxdata = new LinkedList<Robot>();
+        turnNumber = new LinkedList<Integer>();
         teamOfRobot[0] = new Robot(1, teamNumber); //scout
         teamOfRobot[1] = new Robot(2, teamNumber); //sniper
         teamOfRobot[2] = new Robot(3, teamNumber); //tank
@@ -51,6 +58,35 @@ public class RobotTeam {
             }
         }
         return counter;
+    }
+    
+public void addMailbox(Robot r, int play, int numberofteam) {
+    	
+    	if (!turnNumber.isEmpty()) {
+    		for (int p = 0; p < turnNumber.size(); p++) {
+        		if (mailBoxdata.get(p) == r) {
+        			turnNumber.set(p, play + numberofteam * 2);
+        		} else {
+        	    	mailBoxdata.add(r);
+        	    	turnNumber.add(play + numberofteam * 2);
+        		}
+        	}
+    	} else {
+    		mailBoxdata.add(r);
+	    	turnNumber.add(play + numberofteam * 2);
+    	}
+    	
+
+    	for (int j = 0; j < turnNumber.size(); j++) {
+    		if (turnNumber.get(j) == play) {
+    			turnNumber.remove(j);
+    			mailBoxdata.remove(j);
+    		}
+    	}
+    	
+    	for (int i = 0; i < 3; i++) {
+    		teamOfRobot[0].getMailBoxdata(mailBoxdata);
+    	}
     }
 
     public boolean isHuman() {
@@ -83,7 +119,23 @@ public class RobotTeam {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+    	 // TODO Auto-generated method stub
+    	RobotTeam rt1 = new RobotTeam(true, Color.red, 1);
+    	RobotTeam rt2 = new RobotTeam(true, Color.red, 1);
+    	RobotTeam rt3 = new RobotTeam(true, Color.red, 1);
+    	
+    	Robot r = new Robot(2, 0);
+    	Robot r1 = new Robot(1, 0);
+    	Robot r2 = new Robot(3, 0);
+    	
+    	rt1.addMailbox(r, 1, 1);
+    	rt1.addMailbox(r1, 1, 1);
+    	rt1.addMailbox(r2, 1, 1);
+    	
+    	System.out.println(rt1.mailBoxdata.size() + "size");
+    	for (int i = 0; i < rt1.mailBoxdata.size(); i++) {
+    		System.out.println(rt1.mailBoxdata.get(i).health + "health");
+    	}
     }
 
 }
